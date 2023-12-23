9ad42099531fc617b3d88f64e189764dfe1a2717
sonbae <henry@crysis.dev>
2023-12-23 11:56:26 -0800

    simple gui functioning + format

--- gui.py
+++ gui.py
@@ -1,11 +1,27 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QVBoxLayout, QWidget
 import sys
 
 
 class MainWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Drag and Drop")
+        self.file_paths = None
+
+        self.file_paths_label = QLabel("EMPTY WORDS")
+
+        self.reset_button = QPushButton("Reset")
+        self.reset_button.clicked.connect(self.reset_list)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.file_paths_label)
+        layout.addWidget(self.reset_button)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
+
+        self.setWindowTitle("discordge")
         self.resize(720, 480)
         self.setAcceptDrops(True)
 
@@ -15,10 +31,14 @@ class MainWidget(QMainWindow):
         else:
             event.ignore()
 
+    def reset_list(self):
+        print('reset')
+        self.file_paths_label.setText('')
+
     def dropEvent(self, event):
-        files = [u.toLocalFile() for u in event.mimeData().urls()]
-        for f in files:
-            print(f)
+        self.file_paths = list(map(lambda x: x.toLocalFile(), event.mimeData().urls()))
+        print('\n'.join(self.file_paths))
+        self.file_paths_label.setText('\n'.join(self.file_paths))
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -4,44 +4,48 @@ from functools import reduce
 from pathlib import Path
 from cryptography.fernet import Fernet
 
-MAX_UPLOAD_SIZE = 500 # MB
+MAX_UPLOAD_SIZE = 500  # MB
+
 
 def decompose_file(file_path, fer):
-  # open file and read as binary
-  with open(file_path, 'rb') as f:
-    file_binary = f.read()
-    print(size(sys.getsizeof(file_binary)))
+    # open file and read as binary
+    with open(file_path, 'rb') as f:
+        file_binary = f.read()
+        print(size(sys.getsizeof(file_binary)))
 
-    binary_splits = [file_binary[i:i+MAX_UPLOAD_SIZE*1000000] for i in range(0, len(file_binary), MAX_UPLOAD_SIZE*1000000)]
-    print(size(sys.getsizeof(binary_splits)))
+        binary_splits = [file_binary[i:i + MAX_UPLOAD_SIZE * 1000000] for i in
+                         range(0, len(file_binary), MAX_UPLOAD_SIZE * 1000000)]
+        print(size(sys.getsizeof(binary_splits)))
 
-  for i, split in enumerate(binary_splits):
-    print(i)
-    with open(f'tmp/{i}.bin', 'wb') as f:
-      f.write(fer.encrypt(split))
-    
+    for i, split in enumerate(binary_splits):
+        print(i)
+        with open(f'tmp/{i}.bin', 'wb') as f:
+            f.write(fer.encrypt(split))
+
+
 def compose_file_from_parts(parts):
-  with open('example/new.mp4', 'wb') as f:
-    f.write(reduce(lambda x, y: x+y, parts))
+    with open('example/new.mp4', 'wb') as f:
+        f.write(reduce(lambda x, y: x + y, parts))
 
 
 def main():
-  key = Fernet.generate_key().decode('ascii')
-  print(f'Save the key: {key}')
+    key = Fernet.generate_key().decode('ascii')
+    print(f'Save the key: {key}')
 
-  f = Fernet(key)
+    f = Fernet(key)
 
-  decompose_file('example/csgo.mp4', f)
-  yur = input('whats your key you fuck: ')
+    decompose_file('example/csgo.mp4', f)
+    yur = input('whats your key you fuck: ')
 
-  fhelp = Fernet(yur.encode('ascii'))
+    fhelp = Fernet(yur.encode('ascii'))
 
-  path_obj = Path('tmp/')
-  items = path_obj.glob('*')
-  sorted_items = sorted(items, key=lambda item: item.name)
-  parts = [fhelp.decrypt(open(f'tmp/{item.name}', 'rb').read()) for item in sorted_items]
+    path_obj = Path('tmp/')
+    items = path_obj.glob('*')
+    sorted_items = sorted(items, key=lambda item: item.name)
+    parts = [fhelp.decrypt(open(f'tmp/{item.name}', 'rb').read()) for item in sorted_items]
 
-  compose_file_from_parts(parts)
+    compose_file_from_parts(parts)
+
 
 if __name__ == '__main__':
-  main()
+    main()
